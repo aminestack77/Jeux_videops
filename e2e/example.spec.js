fef738184ff1b04201bba14ef7c63d72e03d2fe1
Amine Sokate <aminesokate77@gmail.com>
2026-04-20 13:57:34 +0200

    fix des fix sur les test

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,24 +1,6 @@
-
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-//MEs tests
-
+// VOS TESTS :
 test.describe('Two Ships Passing in the Night', () => {
 
   test.beforeEach(async ({ page }) => {
@@ -31,7 +13,6 @@ test.describe('Two Ships Passing in the Night', () => {
     //  détecte des crashs invisibles
     page.on('pageerror', error => errors.push(error));
 
-
     const canvas = page.locator('canvas');
     await expect(canvas).toBeVisible();
     
@@ -42,5 +23,6 @@ test.describe('Two Ships Passing in the Night', () => {
   test('Le joueur peut démarrer le jeu avec le clavier', async ({ page }) => {
     // Simuler l'appui sur Espace pour passer l'écran titre
     await page.keyboard.press('Space');
+  });
 
 });
